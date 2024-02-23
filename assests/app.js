1723a5acec0e8f389f85d6799a654cce8470ce8f
Kulsumnkhan <121419147+Kulsumnkhan@users.noreply.github.com>
2024-02-23 23:01:38 +0530

    Portfolio

--- assests/app.js
+++ assests/app.js
@@ -20,3 +20,15 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right = '-200px';
 }
+
+{/* <script> */}
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwDT77y5_X9oWCw6uloAnQ80dJn7asL1cdGBA2yf4CJqiM28Qh3P3oJvVLWNV8Dk7mQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+// </script>
